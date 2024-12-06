906c362b403a0cfe2c5ac971863a8fb7c7d4a552
Peter Ehrich <pe.ehrich@gmail.com>
2024-12-06 17:09:26 +0100

    Working on Solution 2024.4.1

--- 2024/day4/1/main.go
+++ 2024/day4/1/main.go
@@ -7,6 +7,88 @@ import (
 	"strings"
 )
 
+var REFERENCE_STRING = "XMAS"
+var WINDOW_LENGTH = len(REFERENCE_STRING)
+
+func slideOver(input string) int {
+	var hits int
+	for i := 0; i < len(input)-WINDOW_LENGTH; i++ {
+		if input[i:i+WINDOW_LENGTH] == REFERENCE_STRING {
+			hits++
+		}
+	}
+	return hits
+}
+
+func traverseHorizontalForward(rows []string) int {
+	copyRows := make([]string, len(rows))
+	copy(copyRows, rows)
+
+	var hits int
+	for _, row := range reverseRows(copyRows) {
+		hits += slideOver(row)
+	}
+	return hits
+}
+
+func traverseHorizontalBackward(rows []string) int {
+	copyRows := make([]string, len(rows))
+	copy(copyRows, rows)
+
+	var hits int
+	for _, row := range copyRows {
+		hits += slideOver(row)
+	}
+	return hits
+}
+
+func traverseVerticalForward(rows []string) int {
+	copyRows := make([]string, len(rows))
+	copy(copyRows, rows)
+
+	var hits int
+	for i := 0; i < len(copyRows[0]); i++ {
+		var column string
+		for _, row := range copyRows {
+			column += string(row[i])
+		}
+		hits += slideOver(column)
+	}
+	return hits
+}
+
+func traverseVerticalBackward(rows []string) int {
+	copyRows := make([]string, len(rows))
+	copy(copyRows, rows)
+
+	var hits int
+	for i := 0; i < len(copyRows[0]); i++ {
+		var column string
+		for _, row := range reverseRows(copyRows) {
+			column += string(row[i])
+		}
+		hits += slideOver(column)
+	}
+	return hits
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func reverseRows(rows []string) []string {
+	var reversedRows []string
+	for _, row := range rows {
+
+		reversedRows = append([]string{reverseString(row)}, reversedRows...)
+	}
+	return reversedRows
+}
+
 func main() {
 
 	file, err := os.ReadFile("input.txt")
@@ -16,6 +98,15 @@ func main() {
 
 	rows := strings.Split(string(file), "\n")
 
-	fmt.Println(rows)
+	thfHits := traverseHorizontalForward(rows)
+	fmt.Println("Hits with traverseHorizontalForward:", thfHits)
 
+	thbHits := traverseHorizontalBackward(rows)
+	fmt.Println("Hits with traverseHorizontalBackward:", thbHits)
+
+	tvfHits := traverseVerticalForward(rows)
+	fmt.Println("Hits with traverseVerticalForward:", tvfHits)
+
+	tvbHits := traverseVerticalBackward(rows)
+	fmt.Println("Hits with traverseVerticalBackward:", tvbHits)
 }
