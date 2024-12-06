b0cfda3e1b49f1f6d7886398617aed2488c20518
Peter Ehrich <pe.ehrich@gmail.com>
2024-12-06 11:59:14 +0100

    Solution for Day 2 - Part 1

--- 2024/2/1/main.go
+++ 2024/2/1/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -28,11 +30,11 @@ func loadLinesFromFile(filePath string) ([]string, error) {
 	return lines, nil
 }
 func parseLines(lines []string) [][]int {
-	
-	vat parseResult [][]int
+
+	var parseResult [][]int
 	for _, line := range lines {
 		stringNumbers := strings.Split(line, " ")
-		arr := make([]int, len(strings.Split(line, " ")))
+		arr := make([]int, len(stringNumbers))
 
 		for j, element := range stringNumbers {
 			num, err := strconv.Atoi(element)
@@ -40,10 +42,34 @@ func parseLines(lines []string) [][]int {
 				fmt.Println("Error converting string to int:", err)
 				continue
 			}
-			arr = append(arr,num)
+			arr[j] = num
+		}
+		parseResult = append(parseResult, arr)
+	}
+	return parseResult
+}
+
+func rowIsSortedAscending(row []int) bool {
+	return slices.IsSorted(row)
+}
+
+func rowIsSortedDescending(row []int) bool {
+	var comparison = make([]int, len(row))
+	copy(comparison, row)
+	slices.Sort(comparison)
+	slices.Reverse(comparison)
+	return slices.Equal(row, comparison)
+}
+
+func checkAdjacentLevels(row []int) bool {
+	for i := 0; i < len(row)-1; i++ {
+		adjacentDistance := int(math.Abs(float64(row[i] - row[i+1])))
+
+		if adjacentDistance < 1 || adjacentDistance > 3 {
+			return false
 		}
 	}
-	return [][]int{column1, column2}
+	return true
 }
 
 func main() {
@@ -54,7 +80,16 @@ func main() {
 		return
 	}
 
-	finalArray := make([]int, len(strings.Split(lines[0], " ")))
+	parsedArray := parseLines(lines)
 
-	fmt.Println(finalArray)
+	validReportCounter := 0
+	for _, row := range parsedArray {
+		if rowIsSortedAscending(row) && checkAdjacentLevels(row) {
+			validReportCounter++
+		} else if rowIsSortedDescending(row) && checkAdjacentLevels(row) {
+			validReportCounter++
+		}
+	}
+
+	fmt.Println(validReportCounter)
 }
